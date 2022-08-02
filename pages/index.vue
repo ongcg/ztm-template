3587a8713c16479b13a21ea119eb33b528e25d2b
ongcg <chengee1005@gmail.com>
2022-08-02 23:13:07 +0800

    Updated homepage with new title and tagline

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <h1 class="text-5xl">Imagination + Innovation, the seeds of Creativity !</h1>
+      <p class="mt-5">Turn your ideas into ......</p>
     </div>
   </div>
 </template>
