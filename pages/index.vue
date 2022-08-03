c2df3287fe520371cc8cd2a3b2cace8b140b21d6
ongcg <chengee1005@gmail.com>
2022-08-03 23:59:31 +0800

    cleanup page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,2 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Imagination + Innovation, the seeds of Creativity !</h1>
-      <p class="mt-5">Turn your ideas into ......</p>
-    </div>
-  </div>
 </template>
